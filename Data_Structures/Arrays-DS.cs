b210984c238b2cd47ddf4f60438e0c85322d3806
giannencljsn <giannejuson16@gmail.com>
2025-03-06 14:05:57 +0800

    +solution Array-DS -

---
+++ Data_Structures/Arrays-DS.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<int> reverseArray(List<int> a)
+    {
+       int n = a.Count; // Use Count for List<int>
+        int[] totalArr = new int[n]; // Create an array to hold reversed elements
+        
+        // Reverse the list
+        for (int i = n - 1; i >= 0; i--)
+        {
+            totalArr[n - 1 - i] = a[i]; // Assign elements in reverse order
+        }
+        
+        // Convert the array to a List<int> and return it
+        return new List<int>(totalArr);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> res = Result.reverseArray(arr);
+
+        textWriter.WriteLine(String.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
